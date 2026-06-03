e736db549b628ef0bbb09a9afed589fac57ed200
Vitória Lima <165972453+vitoriagblima@users.noreply.github.com>
2026-06-03 17:42:51 +0000

    First Commit

---
+++ src/mathematics/Area.java
@@ -0,0 +1,29 @@
+package mathematics;
+
+import java.util.Scanner;
+
+public class Area {
+    
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        Double A = sc.nextDouble();
+        Double B = sc.nextDouble(); 
+        Double C = sc.nextDouble();
+
+        Double areaTriangulo = (A * C) / 2; 
+        Double areaCirculo = 3.14159 * Math.pow(C, 2); 
+        Double areaTrapezio = ((A + B) * C) / 2;
+        Double areaQuadrado = Math.pow(B, 2);
+        Double areaRetangulo = A * B;
+
+        System.out.printf("TRIANGULO: %.3f%n", areaTriangulo);
+        System.out.printf("CIRCULO: %.3f%n", areaCirculo);
+        System.out.printf("TRAPEZIO: %.3f%n", areaTrapezio);
+        System.out.printf("QUADRADO: %.3f%n", areaQuadrado);
+        System.out.printf("RETANGULO: %.3f%n", areaRetangulo);
+
+        sc.close();
+    }
+}
