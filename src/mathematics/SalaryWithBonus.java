0ffb0c512b6de496d3d17f99339ce25af240ce69
Vitória Lima <165972453+vitoriagblima@users.noreply.github.com>
2026-06-04 22:48:48 +0000

    First Commit

---
+++ src/mathematics/SalaryWithBonus.java
@@ -0,0 +1,22 @@
+package mathematics;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Scanner;
+
+public class SalaryWithBonus {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String sellerName = sc.nextLine();
+        BigDecimal fixedSalary = sc.nextBigDecimal();
+        BigDecimal salesTotal = sc.nextBigDecimal();
+
+        BigDecimal commission = salesTotal.multiply(new BigDecimal("0.15"));
+        BigDecimal finalSalary = fixedSalary.add(commission);
+
+        System.out.println("TOTAL = R$ " + finalSalary.setScale(2, RoundingMode.HALF_UP));
+
+        sc.close();
+    }
+}
