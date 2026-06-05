f497fe107e692ff4d3d43e07c0a35fa64071fb17
Vitória Lima <165972453+vitoriagblima@users.noreply.github.com>
2026-06-05 17:40:54 +0000

    feat: implement Beecrowd exercise 1013 solution

---
+++ src/beginner/P1014_Consumption.java
@@ -0,0 +1,18 @@
+package beginner;
+
+import java.util.Scanner;
+
+public class P1014_Consumption {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int X = sc.nextInt();
+        Double Y = sc.nextDouble();
+
+        Double total = X / Y;
+
+        System.out.printf("%.3f km/l\n", total);
+
+        sc.close();
+    }
+}
