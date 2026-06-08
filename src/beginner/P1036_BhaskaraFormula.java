6357f18c216f58eaff05dc278f20e8ad4e4ca42f
Vitória Lima <165972453+vitoriagblima@users.noreply.github.com>
2026-06-08 19:21:40 +0000

    feat: solve beecrowd 1036 in java

---
+++ src/beginner/P1036_BhaskaraFormula.java
@@ -0,0 +1,27 @@
+package beginner;
+
+import java.util.Scanner;
+
+public class P1036_BhaskaraFormula {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double A = sc.nextDouble();
+        double B = sc.nextDouble();
+        double C = sc.nextDouble();
+
+        double delta = B * B - 4 * A * C;
+
+        if (A == 0 || delta < 0) {
+            System.out.println("Impossivel calcular");
+        } else {
+            double R1 = (-B + Math.sqrt(delta)) / (2 * A);
+            double R2 = (-B - Math.sqrt(delta)) / (2 * A);
+
+            System.out.printf("R1 = %.5f%n", R1);
+            System.out.printf("R2 = %.5f%n", R2);
+        }
+
+        sc.close();
+    }
+}
