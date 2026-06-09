aa809a915287c134530a65c16cfcbc819848e55c
Vitória Lima <165972453+vitoriagblima@users.noreply.github.com>
2026-06-09 13:04:32 +0000

    style: fix Java file indentation

--- src/beginner/P1040_AverageIII.java
+++ src/beginner/P1040_AverageIII.java
@@ -4,39 +4,39 @@ import java.util.Scanner;
 
 public class P1040_AverageIII {
     public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-                    // float is used because the grades and averages can contain decimal values.
-                            float N1 = sc.nextFloat();
-                                    float N2 = sc.nextFloat();
-                                            float N3 = sc.nextFloat();
-                                                    float N4 = sc.nextFloat();
+        // float is used because the grades and averages can contain decimal values.
+        float N1 = sc.nextFloat();
+        float N2 = sc.nextFloat();
+        float N3 = sc.nextFloat();
+        float N4 = sc.nextFloat();
 
-                                                            float average = ((N1 * 2) + (N2 * 3) + (N3 * 4) + (N4 * 1)) / 10;
+        float average = ((N1 * 2) + (N2 * 3) + (N3 * 4) + (N4 * 1)) / 10;
 
-                                                                    System.out.printf("Media: %.1f%n", average);
+        System.out.printf("Media: %.1f%n", average);
 
-                                                                            if (average >= 7.0) {
-                                                                                        System.out.println("Aluno aprovado.");
-                                                                                                } else if (average < 5.0) {
-                                                                                                            System.out.println("Aluno reprovado.");
-                                                                                                                    } else {
-                                                                                                                                System.out.println("Aluno em exame.");
+        if (average >= 7.0) {
+            System.out.println("Aluno aprovado.");
+        } else if (average < 5.0) {
+            System.out.println("Aluno reprovado.");
+        } else {
+            System.out.println("Aluno em exame.");
 
-                                                                                                                                            float examGrade = sc.nextFloat();
-                                                                                                                                                        System.out.printf("Nota do exame: %.1f%n", examGrade);
+            float examGrade = sc.nextFloat();
+            System.out.printf("Nota do exame: %.1f%n", examGrade);
 
-                                                                                                                                                                    float finalAverage = (average + examGrade) / 2;
+            float finalAverage = (average + examGrade) / 2;
 
-                                                                                                                                                                                if (finalAverage >= 5.0) {
-                                                                                                                                                                                                System.out.println("Aluno aprovado.");
-                                                                                                                                                                                                            } else {
-                                                                                                                                                                                                                            System.out.println("Aluno reprovado.");
-                                                                                                                                                                                                                                        }
+            if (finalAverage >= 5.0) {
+                System.out.println("Aluno aprovado.");
+            } else {
+                System.out.println("Aluno reprovado.");
+            }
 
-                                                                                                                                                                                                                                                    System.out.printf("Media final: %.1f%n", finalAverage);
-                                                                                                                                                                                                                                                            }
+            System.out.printf("Media final: %.1f%n", finalAverage);
+        }
 
-                                                                                                                                                                                                                                                                    sc.close();
-                                                                                                                                                                                                                                                                        }
-                                                                                                                                                                                                                                                                        }
+        sc.close();
+    }
+}
